640dce2d250394ed2800f7cf0ca8930cfe10b919
Oliver Greisdorfer <oliver.greisdorfer@hotmail.com>
2019-08-20 13:13:27 +0200

    add structure

---
+++ intermediate/src/container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import "./container.css";
+class Container extends Component {
+    render() {
+        return (
+            <div id="container"> Im the Container   </div>
+        );
+    }
+}
+
+export default Container;
--- intermediate/src/index.js
+++ intermediate/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from "./header";
 import Footer from "./footer";
+import Container from "./container";
 
-ReactDOM.render(<div> <Header />
+ReactDOM.render(<div> <Header /><Container />
 	<Footer /> </div>
 	, document.getElementById('root'));
