dfcc9d2c24add70a9d7599d280832166ad690353
Oliver Greisdorfer <oliver.greisdorfer@hotmail.com>
2019-08-20 11:09:55 +0200

    123

---
+++ intermediate/src/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./footer.css";
+class Footer extends Component {
+    render() {
+        return (
+            <footer>I'm the Footer!
+         </footer>
+        );
+    }
+}
+
+export default Footer;
